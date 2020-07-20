12c449b4da64918788cb7497833f9c22637318ff
npoet <npoet@umich.edu>
2020-07-20 01:06:58 -0400

    add lines attr, simple output fn

--- src/elements/poem.py
+++ src/elements/poem.py
@@ -7,3 +7,8 @@ class Poem(Rule):
     def __init__(self, name: str):
         super().__init__(name)
         self.num_lines: int
+        self.lines = []
+
+    def write_poem(self):
+        for line in self.lines:
+            print(line, "\n")
