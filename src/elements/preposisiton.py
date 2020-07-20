58a62595a1e0446594ccf1068bae39be5c49340f
npoet <npoet@umich.edu>
2020-07-20 02:04:52 -0400

    end_lst -> rule_lst, add populate_rule_lst() fn

--- src/elements/preposisiton.py
+++ src/elements/preposisiton.py
@@ -7,9 +7,13 @@ class Preposition(Rule):
     def __init__(self, name: str):
         super().__init__(name)
         self.wordlist = []
-        self.end_list = []
+        self.rule_lst = []
 
     def populate_wordlist(self, words: str):
         assert len(self.wordlist) == 0
         split_words = words.split("|")
         self.wordlist = [word for word in split_words]
+
+    def populate_rule_lst(self, rules: str):
+        split_rules = rules.split("|")
+        self.rule_lst = [rule for rule in split_rules]
