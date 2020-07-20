b5f60d7ca3600df2eed1799e54a63329a55715f0
npoet <npoet@umich.edu>
2020-07-20 00:53:53 -0400

    update rule base class funcs, properties

--- src/elements/rule.py
+++ src/elements/rule.py
@@ -1,1 +1,22 @@
-"""Class for rule element handling"""
+"""Rule element class"""
+
+import random
+
+
+class Rule(object):
+    raw_definition = ''
+    definition_lst = []
+
+    def __init__(self, name: str):
+        self.name = name
+
+    def raw_def_to_list(self):
+        assert self.raw_definition is not None
+        split_string = self.raw_definition.split(" ")
+        # remove all empty strings from str split (leading and trailing spaces etc)
+        lst = [x for x in split_string if x != '']
+        self.definition_lst = lst
+
+    @staticmethod
+    def select_rand(lst: [str]):
+        return random.choice(lst)
