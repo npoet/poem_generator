0bf051e43f41afcd4ab62b1507b3d8363b94b230
npoet <npoet@umich.edu>
2020-07-20 00:35:16 -0400

    add wordlist obj and populate_wordlist()

--- src/elements/preposisiton.py
+++ src/elements/preposisiton.py
@@ -6,3 +6,10 @@ from .rule import Rule
 class Preposition(Rule):
     def __init__(self, name: str):
         super().__init__(name)
+
+    wordlist = []
+
+    def populate_wordlist(self, lst: [str]):
+        assert len(self.wordlist) == 0
+        split_words = lst.split("|")
+        self.wordlist = [word for word in split_words]
