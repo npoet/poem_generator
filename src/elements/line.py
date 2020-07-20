c6525eaf296d37126eb2e97ab474cb248e32b09a
npoet <npoet@umich.edu>
2020-07-20 02:05:30 -0400

    add rules fns, rule_lst, selected rule

--- src/elements/line.py
+++ src/elements/line.py
@@ -7,4 +7,15 @@ class Line(Rule):
     def __init__(self, name: str):
         super().__init__(name)
         self.name = name
-        self.content: [str]
+        self.content = []
+        self.rule_lst = []
+        self.rule = ''
+
+    def populate_rule_lst(self, rules: str):
+        assert len(self.rule_lst) == 0
+        split_rules = rules.split("|")
+        self.rule_lst = [rule for rule in split_rules]
+
+    def pick_rule(self):
+        assert self.rule_lst != []
+        self.rule = self.select_rand(self.rule_lst)[1:-1].lower().capitalize()
