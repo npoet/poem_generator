492d1adbb4f2a1fd6d090b2387dfda56b79546cb
npoet <npoet@umich.edu>
2020-07-20 18:09:40 -0400

    remove extra attr def

--- src/elements/line.py
+++ src/elements/line.py
@@ -6,7 +6,6 @@ from .rule import Rule
 class Line(Rule):
     def __init__(self, name: str):
         super().__init__(name)
-        self.name = name
         self.rule_lst = []
         self.rule = ''
 
