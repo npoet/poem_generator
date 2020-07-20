db0f7bc803a09a0ee52971778f2341e12d4ed7fa
npoet <npoet@umich.edu>
2020-07-20 04:29:18 -0400

    add raw_to_name(), update logic for test to console

--- src/poem_generator.py
+++ src/poem_generator.py
@@ -4,6 +4,12 @@ import re
 from elements import *
 
 
+def raw_to_name(word: str):
+    if word in KEYWORDS:
+        return word
+    return word[1:-1].lower().capitalize()
+
+
 def main():
     # encapsulate file open for closure on clean-up
     with open("data/input.txt", "r") as f:
@@ -36,11 +42,35 @@ def main():
             # populate wordlist where applicable
             if rule_name in SENTENCE_ELEMENTS:
                 cls.populate_wordlist(cls.definition_lst[0])
+                cls.populate_rule_lst(cls.definition_lst[1])
             elements.append(cls)
 
         # create poem structure, find poem in list by attr
         poem = next(elem for elem in elements if elem.name == 'Poem')
+        line = next(elem for elem in elements if elem.name == 'Line')
+        line.populate_rule_lst(line.definition_lst[0])
         poem.num_lines = len(poem.definition_lst)
+        # create lines for poem
+        for i in range(poem.num_lines):
+            line_start_name = raw_to_name(line.select_rand(line.rule_lst))
+            line_start_obj = next(elem for elem in elements if elem.name == line_start_name)
+            line_start_word = line_start_obj.select_rand(line_start_obj.wordlist)
+            line_start_rule = raw_to_name(line_start_obj.select_rand(line_start_obj.rule_lst))
+            poem.lines.append({i: [line_start_word, line_start_rule]})
+
+        # finish all lines
+        for i in range(len(poem.lines)):
+            while list(poem.lines[i].values())[0][-1] not in KEYWORDS:
+                word = list(poem.lines[i].values())[0][-1]
+                next_obj = next(elem for elem in elements if elem.name == word)
+                next_word = next_obj.select_rand(next_obj.wordlist)
+                next_rule = raw_to_name(next_obj.select_rand(next_obj.rule_lst))
+                poem.lines[i][list(poem.lines[i].keys())[0]].pop(-1)
+                poem.lines[i][list(poem.lines[i].keys())[0]].append(next_word)
+                poem.lines[i][list(poem.lines[i].keys())[0]].append(next_rule)
+
+        # write poem
+        poem.write_poem_to_console()
 
     return
 
